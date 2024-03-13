b9ab0790d29dffac9ffa85986ef5d410057346b8
tejaswini132004 <tejaswinimoily@gmail.com>
2024-03-13 20:40:06 +0530

    13/03/24 Contact Us

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import About from "./Pages/About";
+import Contact from "./Pages/Contact";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <About />
+      {/* <About /> */}
+      <Contact/>
     </>
   );
 }
