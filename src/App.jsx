f4d5082bb1847c85fa0f8090bba6545376e54022
Aaditya Kulkarni <aadikulkarni727@gmail.com>
2024-03-17 10:31:50 +0530

    Updated Navbar and started with Home page (17-03-24)

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import About from "./Pages/About";
+import './App.css'
+import Navbar from './Components/Navbar/Navbar.jsx'
+import {BrowserRouter, Routes , Route } from 'react-router-dom'
+import About from './Pages/About.jsx'
+import ContatcUs from './Pages/ContatcUs.jsx'
+import Home from './Pages/Home.jsx'
+import HowItWorks from './Pages/HowItWorks.jsx'
+import LoginSignUp from './Pages/LoginSignUp.jsx'
+import OurTeam from './Pages/OurTeam.jsx'
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <About />
-    </>
+    <div>
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path= "/" element={<Home category='home'/>} />
+        <Route path= "/about" element={<About  category="about" />} />
+        <Route path= "/ourTeam" element={<OurTeam  category="ourTeam" />} />
+        <Route path= "/howItWorks" element={<HowItWorks  category="howItWorks" />} />
+        <Route path= "/contactUs" element={<ContatcUs category="contactUs"/>} />
+        <Route path= "/login" element={<LoginSignUp />} />
+       </Routes>
+      </BrowserRouter>
+      
+      
+    </div>
   );
 }
 
-export default App;
+export default App
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,77 @@
+import React from 'react'
+import './CSS/home.css'
+// import page1 from '../Components/assets/images/page1.png'
+// import page2 from '../Components/assets/images/page2.png'
+import page1 from '../Components/assets/images/page1.png'
+import page2 from '../Components/assets/images/page2.jpg'
+
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
-  )
+    <div className="m-auto p-auto">
+      <div className="w-80vw m-auto mt-2">
+        <div>
+         <div className="pl-48 pt-32 md: py-5p px-15p">
+           <h1 className=" text-[#f6df0f] text-7xl font-black "><b>PRESSX</b></h1>  
+           <h2 className="text-3xl font-semibold ">Ironing out the Wrinkles of Life</h2><br></br> 
+           <h4>A website / app for everyone, designed to improve your laundry experience.</h4><br></br>
+         </div>  
+      </div>
+      <div >
+                 <div>
+                     <img  src={page1} alt="img1" />
+                     <div >
+                         <img  src={page2} alt="img2" />
+                     </div>
+                 </div>
+             </div>
+
+      </div >
+     </div>
+  );
 }
 
 export default Home
+
+// import React from "react";
+// import "./CSS/home.css";
+
+// const Home = () => {
+//   return (
+//     <div id="container">
+//         <div className="row">
+//             <div>
+//                 <div className="content">
+//                     <h1 className="heading"><b>PRESSX</b></h1>
+//                     <h2 className="subHeading">Ironing out the Wrinkles of Life</h2><br>
+//                     <h4>A website / app for everyone, designed to improve your laundry experience.</h4><br>
+//                     {/* <!-- <ul className="download-main list-unstyled">
+//                         <li>
+//                             <a href="#" className="play-store-footer">
+//                                 <img className="download-img" src="images/play store.png"
+//                                     alt="Play Store Download">
+//                             </a>
+//                         </li>
+//                         <li>
+//                             <a href="#" className="app-store-footer">
+//                                 <img className="download-img" src="images/app store.png"
+//                                     alt="App Store Download">
+//                             </a>
+//                         </li>
+//                     </ul> --> */}
+//                 </div>
+//             </div>
+//             <div >
+//                 <div>
+//                     <img className="machinePic" src="images\page1.png" alt="img1">
+//                     <div className="smallPicSection">
+//                         <img className="smallPic" src="images\page2.jpg" alt="img2">
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default Home
